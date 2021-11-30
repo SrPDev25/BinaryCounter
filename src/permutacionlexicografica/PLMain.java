3637cb4c71b37c9893f283f080b890d1cdc87604
SrPollo25 <daniel.ortega.alu@santamaria-artesano.org>
2021-11-30 14:11:01 +0100

    Conseguí el problema de los numeros

--- src/permutacionlexicografica/PLMain.java
+++ src/permutacionlexicografica/PLMain.java
@@ -7,9 +7,21 @@ public class PLMain {
 
     public static void main(String[] args) {
         // TODO code application logic here
-        NumerosBinarios num =new NumerosBinarios();
-        num.getCeros();
-        num.Opciones(9);
+        String[] letras = {"A", "B", "C", "D", 0, 0, 0, 0, 0, 0};
+        int x = 1000000000, y, z = 0;
+        int exponente, sistemaNum=10;
+        while (x != -1) {
+            y = x;
+            z = 0;
+            exponente = 0;
+            while (y >= 1) {
+                z += (y % sistemaNum) * Math.pow(10, exponente);
+                y /= sistemaNum;
+                exponente++;
+            }
+            System.out.println(z);
+            x--;
+        }
     }
     
 }

806d3ea54c2174f465cca2bfaec9517ab0c8328d
SrPollo25 <daniel.ortega.alu@santamaria-artesano.org>
2021-12-01 12:20:47 +0100

    añadi el codigo que funcionaba con numeros en un comentario

--- src/permutacionlexicografica/PLMain.java
+++ src/permutacionlexicografica/PLMain.java
@@ -1,13 +1,39 @@
-
 package permutacionlexicografica;
-
 
 public class PLMain {
 
+    private static void pTabla(String tabla[]){
+        short count=10;
+        while (count >= 0) {
+                System.out.print(tabla[count]);
+                count--;
+            }
+        System.out.print("\n");
+    }
 
     public static void main(String[] args) {
         // TODO code application logic here
-        String[] letras = {"A", "B", "C", "D", 0, 0, 0, 0, 0, 0};
+        String[] letras = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J"};
+        String[] printR = {"A","A","A","A","A","A","A","A","A","A"};
+        int x = 1000000000, y, z;
+        int exponente, sistemaNum = 10;
+        while (x != -1) {
+            y = x;
+            z = 0;
+            exponente = 0;
+            while (y >= 1) {
+                printR[exponente] = letras[y % sistemaNum];
+                y /= sistemaNum;
+                exponente++;
+            }
+            pTabla(printR);
+            x--;
+        }
+    }
+
+}
+/*Scanner entrada = new Scanner(System.in);
+        int[] letras = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
         int x = 1000000000, y, z = 0;
         int exponente, sistemaNum=10;
         while (x != -1) {
@@ -21,7 +47,4 @@ public class PLMain {
             }
             System.out.println(z);
             x--;
-        }
-    }
-    
-}
+        }*/
